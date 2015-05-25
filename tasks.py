04b5140fc97c95a8a0ba473ecadb74de63a585a0
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-05-26 00:49:18 +0200

    Enhance the job progress messaging.

--- tasks.py
+++ tasks.py
@@ -11,12 +11,18 @@ from shoptet_catalog import download_shoptet_catalog_to_mongo
 mongo_uri = os.environ.get('MONGOLAB_URI')
 
 def update_ed_catalog():
+    job = get_current_job()
+    def update_job_progress(state):
+        job.meta['progress'] = 'obtaining link to the catalog XML'
+        job.save()
+    
     with Connection(worker.redis_client):
         start = time.time()
-        job = get_current_job()
         login = os.environ.get('ED_LOGIN')
         password = os.environ.get('ED_PASSWORD')
+        set_job_progress('obtaining link to the catalog XML')
         catalog_request_url = os.environ.get('ED_CATALOG_URI')
+        set_job_progress('downloading the catalog XML')
         catalog_url = get_ed_catalog_url(catalog_request_url, login, password)
         def counter_report(counter):
             job.meta['total_items'] = counter.total
@@ -24,6 +30,7 @@ def update_ed_catalog():
             job.save()
         counter = Counter(report=counter_report, report_period=1000)
         download_ed_catalog_to_mongo(mongo_uri, catalog_url, counter)
+        set_job_progress('catalog processed')
 
         end = time.time()
         job.meta['elapsed_time'] = '%.3f sec' % (end - start)
