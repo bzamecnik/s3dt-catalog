4fccc5e388b0e2318c42d401b1a5317badb36e01
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-05-26 01:02:35 +0200

    Fix a bug in setting the job progress.

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ mongo_uri = os.environ.get('MONGOLAB_URI')
 def update_ed_catalog():
     job = get_current_job()
     def set_job_progress(state):
-        job.meta['progress'] = 'obtaining link to the catalog XML'
+        job.meta['progress'] = state
         job.save()
     
     with Connection(worker.redis_client):
