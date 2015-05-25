63dd9af93081859f2e31bbb17b938977b4ae1f6f
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-05-26 00:04:22 +0200

    Download the catalog from ED and Shoptet in background jobs into MongoDB.
    Use RQ (redis queue) to manage the background jobs.
    Add a Heroku worker process.
    Store the catalog data into MongoDB, generate the export XML from there.
    Integrate RQ dashboard and a custom dashboard with detailed job state and progress information.
    Rewrite the UI.
    Remove XML validation.

--- app.py
+++ app.py
@@ -2,198 +2,95 @@
 Flask Documentation:     http://flask.pocoo.org/docs/
 Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-
-This file creates your application.
 """
 
-import csv
-from flask import Flask, render_template, request, redirect, make_response
-from io import BytesIO, StringIO
-from lxml import etree
+import arrow
+from collections import OrderedDict
+from flask import Flask, render_template, redirect, url_for, make_response
 import os
-import pandas as pd
-import re
-import requests
-from werkzeug.contrib.iterio import IterIO
-from werkzeug.datastructures import Headers
-from werkzeug.wsgi import wrap_file
-import xmltodict
-from zipfile import ZipFile
+from pymongo import MongoClient
+import redis
+from rq import Queue, Connection
+from rq.registry import FinishedJobRegistry, StartedJobRegistry
+from rq_dashboard import RQDashboard
+import worker
 
-# local modules
-import catalog
-import xml_validation
+from export_catalog import export_catalog_xml
+import tasks
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
-    'this should be set via an ENV variable')
-app.config['ED_CONFIG'] = {
-    # 'catalog_request_url': 'http://localhost:5000/ed_system_catalog_response_zip.xml',
-    'catalog_request_url': 'http://public.ws.cz.elinkx.biz/service.asmx/getProductListDownloadZIP',
-    'login': os.environ.get('ED_LOGIN'),
-    'password': os.environ.get('ED_PASSWORD')}
-app.config['SHOPTET_CONFIG'] = {
-     # 'existing_products_url': 'http://localhost:5000/shoptet_product_codes.csv',
-    'existing_products_url': 'http://eshop.svet-3d-tisku.cz/export/products.csv?visibility=-1&patternId=2&hash=a2106697936ebe62acc68776868370732a89984fd7f48ac348f0e33cd3837489',
-}
+# Redis Queue dashboard, available at /rq/
+# Make RQDashboard use the Redis-to-go service if available.
 
-###
-# Routing for your application.
-###
+app.config['REDIS_URL'] = worker.redis_url
+RQDashboard(app)
+
+redis_client = worker.redis_client
 
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('home.html')
 
-@app.route('/download_ed/', methods=['POST'])
-def download_ed_catalog():
-#     response_xml = '''<?xml version="1.0" encoding="utf-8"?>
-# <ResponseProductListStatus xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.elinkx.cz">
-#   <Status>
-#     <StatusCode>DONE</StatusCode>
-#     <ErrorText />
-#   </Status>
-#   <ProductListStatus>
-#     <url>http://public.ws.cz.elinkx.biz/download/priceList_1055541_UTF8_72d69218-5a07-4654-970d-eaedbc0a56e8.zip</url>
-#     <fileName>priceList_1055541_UTF8_72d69218-5a07-4654-970d-eaedbc0a56e8.zip</fileName>
-#     <isReady>true</isReady>
-#   </ProductListStatus>
-# </ResponseProductListStatus>'''
-    catalog_url = get_ed_catalog_url()
-    catalog_res = requests.get(catalog_url, stream=True)
-    
-    # Read the request content, zip file and XML file via a stream to prevent
-    # reading the whole data into memory at once. The memory used should be
-    # constant regardless of the size of the input data.
-    if not catalog_url.endswith('.zip'):
-        catalog_xml = IterIO(catalog_res.iter_lines)
-    else:
-        with ZipFile(IterIO(catalog_res.iter_content(chunk_size=4096), sentinel=b'')) as zf:
-            with zf.open(zf.filelist[0].filename, 'r') as xml_file:
-                catalog_xml = xml_file
-    
-    filter_enabled = request.form['filter'] == 'true'
-    filtered_catalog = catalog.filter_catalog(catalog_xml) \
-        if filter_enabled \
-        else catalog_xml
+@app.route('/update_ed_catalog/')
+def update_ed_catalog():
+    with Connection(redis_client):
+        q = Queue()
+        result = q.enqueue(tasks.update_ed_catalog)
+        return redirect(url_for('jobs'))
 
-    # TODO: add datetime to the file name
-    headers = Headers()
-    headers.add("Content-Disposition", "attachment; filename=ed_catalog.xml")
-    # allow the output file to be streamed
-    return Response(wrap_file(environ, filtered_catalog),
-        direct_passthrough=True,
-        headers=headers)
+@app.route('/update_shoptet_catalog/')
+def update_shoptet_catalog():
+    with Connection(redis_client):
+        q = Queue()
+        result = q.enqueue(tasks.update_shoptet_catalog)
+        return redirect(url_for('jobs'))
 
-def get_ed_catalog_url():
-    ed_config = app.config['ED_CONFIG']
-    catalog_request_url = ed_config['catalog_request_url']
-    params = {
-        'login': ed_config['login'],
-        'password': ed_config['password'],
-        'encoding': 'UTF-8',
-        'onStock': 'False'}
-    res = requests.get(catalog_request_url, params=params)
-    response_xml = xmltodict.parse(res.text)
-    plStatus = response_xml['ResponseProductListStatus']['ProductListStatus']
-    catalog_url = plStatus['url']
-    return catalog_url
-
-@app.route('/convert_ed_to_shoptet/', methods=['POST'])
-def convert_catalog():
-    # required
-    ed_catalog = request.files['ed_catalog_file']
-    # optional - if not given all products are treated as new
-    shoptet_catalog_file = request.files['shoptet_catalog_file']
-    shoptet_existing_items = shoptet_catalog_file.read().decode()
-    
-    if len(shoptet_existing_items) > 0:
-        df = pd.read_csv(StringIO(shoptet_existing_items),
-            dtype={'product_code': str, 'visible': bool})
-        df.set_index('product_code', inplace=True)
-    else:
-        df = pd.DataFrame()
-    
-    shoptet_catalog = catalog.convert_catalog(ed_catalog, df)
-    response = make_response(shoptet_catalog)
-    # TODO: add datetime to the file name
-    response.headers["Content-Disposition"] = "attachment; filename=shoptet_catalog_import.xml"
+@app.route('/export_catalog/')
+def export_catalog():
+    catalog_xml = export_catalog_xml()
+    response = make_response(catalog_xml)
+    date = arrow.get().format('YYYY-MM-DD_HH-mm-ss')
+    file_name = 'shoptet_catalog_import_%s.xml' % date
+    response.headers["Content-Disposition"] = "attachment; filename=%s" % file_name
     return response
 
-@app.route('/validate_shoptet/', methods=['POST'])
-def validate_shoptet_catalog():
-    doc = request.files['input_file']
-    file_name = doc.filename
-    schema = xml_validation.relax_ng_schema('resources/products-supplier-v10.rng')
-    try:
-        xml_validation.validate_relax_ng(doc, schema)
-        is_valid, log, exception = True, None, None
-    except etree.DocumentInvalid as e:
-        is_valid, log, exception = False, schema.error_log, e
-    
-    return render_template('validate_shoptet.html',
-        file_name=file_name, is_valid=is_valid, exception=exception, log=log)
+@app.route('/jobs/<job_id>')
+def cancel_job(job_id):
+    with Connection(redis_client):
+        q = Queue()
+        job = q.fetch_job(job_id)
+        return render_template('job_details.html', job=job)
 
-@app.route('/download_shoptet/existing_catalog/')
-def download_shoptet_existing_products():
-    '''
-    Converts one CSV file to another.
-    '''
-    # example excerpt of the input CSV file (cp1250, CR-LF, quotes):
-    #
-    # code;pairCode;name;productVisibility;
-    # "0001";;"3D tiskarna Profi3DMaker tryska ? 0,5mm / 3D Printer 0,5 mm";"visible";
-    #
-    # example output CSV (UTF-8, LF, no-quotes):
-    # product_code,visible
-    # 0001,true
-    
-    url = app.config['SHOPTET_CONFIG']['existing_products_url']
-    catalog_res = requests.get(url, stream=True)
-    codes_csv = catalog_res.content.decode('cp1250')
-    lines = codes_csv.split('\r\n')
-    
-    existing_products = StringIO()
-    csvwriter = csv.writer(existing_products)
-    csvwriter.writerow(['product_code', 'visible'])
+@app.route('/jobs/<job_id>/cancel')
+def job_details(job_id):
+    with Connection(redis_client):
+        q = Queue()
+        job = q.fetch_job(job_id)
+        job.cancel()
+        return redirect(url_for('jobs'))
 
-    # input columns: code;pairCode;name;productVisibility;
-    # output columns: product_code,visible
-    for row in csv.reader(lines[1:], delimiter=';'):
-        # product id, visibility
-        if len(row) >= 3:
-            csvwriter.writerow([row[0], str(row[3] == 'visible').lower()])
-
-    response = make_response(existing_products.getvalue())
-    # TODO: add datetime to the file name
-    response.headers["Content-Disposition"] = "attachment; filename=shoptet_existing_catalog.csv"
-    return response
-
-@app.route('/<file_name>.xml')
-def send_xml_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.xml'
-    return app.send_static_file(file_dot_text)
+@app.route('/jobs/')
+def jobs():
+    with Connection(redis_client):
+        q = Queue()
+        # possibly also use DeferredJobRegistry()
+        jobs = OrderedDict([
+            (name,
+            [q.fetch_job(job_id) for job_id in registry.get_job_ids()]
+            ) for (name, registry) in
+            [('Waiting', q),
+            ('Running', StartedJobRegistry()),
+            ('Finished', FinishedJobRegistry()),
+            ('Failed', Queue('failed'))]])
+        return render_template('jobs.html', jobs=jobs)
 
 @app.route('/<file_name>.zip')
 def send_zip_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.zip'
     return app.send_static_file(file_dot_text)
-
-
-@app.route('/<file_name>.csv')
-def send_csv_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.csv'
-    return app.send_static_file(file_dot_text)
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
---
+++ ed_catalog.py
@@ -0,0 +1,213 @@
+from collections import OrderedDict
+from decimal import Decimal, ROUND_HALF_UP
+import os
+from pymongo import MongoClient
+import re
+import requests
+from werkzeug.contrib.iterio import IterIO
+import xmltodict
+from zipfile import ZipFile
+
+def download_ed_catalog(catalog_url):
+    print('download_ed_catalog({})', catalog_url)
+    catalog_res = requests.get(catalog_url, stream=True)
+    print('response: {}', catalog_res)
+    
+    # Read the request content, zip file and XML file via a stream to prevent
+    # reading the whole data into memory at once. The memory used should be
+    # constant regardless of the size of the input data.
+    if not catalog_url.endswith('.zip'):
+        catalog_xml = IterIO(catalog_res.iter_lines)
+    else:
+        with ZipFile(IterIO(catalog_res.iter_content(chunk_size=4096), sentinel=b'')) as zf:
+            with zf.open(zf.filelist[0].filename, 'r') as xml_file:
+                catalog_xml = xml_file
+    return catalog_xml
+
+def get_ed_catalog_url(catalog_request_url, login, password):
+    params = {
+        'login': login,
+        'password': password,
+        'encoding': 'UTF-8',
+        'onStock': 'False'}
+    res = requests.get(catalog_request_url, params=params)
+    print(res.text)
+    response_xml = xmltodict.parse(res.text)
+    plStatus = response_xml['ResponseProductListStatus']['ProductListStatus']
+    catalog_url = plStatus['url']
+    return catalog_url
+
+def process_catalog(input_xml, process_item):
+    print('process_catalog()')
+    '''
+    Processes each product item from an ED catalog XML in a streamed way.
+    input_xml - a string or stream representing the XML
+    process_item - a function to be called for each item (OrderedDict)
+    '''
+    def handle_item(path, item, \
+        expected_path=['ResponseProductList', 'ProductList', 'Product']):
+        if [key for (key, value) in path] == expected_path:
+            process_item(item)
+        return True
+    
+    xmltodict.parse(input_xml, item_depth=3, item_callback=handle_item)
+
+def is_3d_print_item(item):
+    'Filters products from just a single category'
+    return item['CommodityName'] == '3D TISK' \
+        or item['CommodityCode'] == '3DP'
+
+def update_item_to_mongo(ed_item, collection):
+    # NOTE: upsert is much slower than insert, but we'd like to
+    # store also items from Shoptet in the same collection
+    print('updating item', ed_item['Code'])
+    shoptet_item = convert_item(ed_item)
+    collection.update(
+        {'code': ed_item['Code']},
+        {'$set': {
+            'code': ed_item['Code'],
+            'ed': ed_item,
+            'shoptet_from_ed': shoptet_item}},
+        upsert=True)
+
+def load_catalog_to_mongo(input_xml, item_collection, counter):
+    def process_item(item):
+        counter.item_visited()
+        if is_3d_print_item(item):
+            counter.item_selected()
+            update_item_to_mongo(item, item_collection)
+    process_catalog(input_xml, process_item)
+    counter.finished()
+
+def convert_item(item):
+    '''Converts an item from ED format to Shoptet format'''
+    
+    endUserPrice = Decimal(item['EndUserPrice'])
+    vatPercent = Decimal(item['Vat']).quantize(
+        Decimal('1'), rounding=ROUND_HALF_UP)
+    vat = Decimal('0.01') * vatPercent
+    endUserPriceWithVat = add_vat(endUserPrice, vat)
+
+    # purchase price without VAT
+    purchasePrice = Decimal(item['YourPriceWithFees'])
+    purchasePriceWithVat = add_vat(purchasePrice, vat)
+    
+    # ED quantizes some stock amounts to ranges, eg. '10-49',
+    # '50-99', '100+', also exact quantities are decimal, eg. '12,00'.
+    # However, Shoptet only accepts exact integer amounts.
+    # So we need to strip the decimal places and approximate the
+    # integer amount, eg. by taking the  minimum if of the range.
+    # Examples:
+    # '12,00' -> '12'
+    # '10-49' -> '10'
+    # '100+' -> '100'
+    stockItemCount = re.sub(r'([0-9]+)[,+-].*', '\\1', item['OnStockText'])
+    
+    # Shoptet only allows EAN-13, not EAN-14. ED EAN codes might
+    # have 13, 14 or even 6 digits. Longer codes are ignored.
+    eanCode = item['EANCode']
+    if not eanCode or len(eanCode) > 14:
+        eanCode = ''
+    elif len(eanCode) == 14:
+        eanCode = ean14_to_ean_13(eanCode)
+
+    out_item = OrderedDict([
+        ('NAME', item['Name']),
+        # ('SHORT_DESCRIPTION', ''), # ?
+        ('DESCRIPTION', item['Description']),
+        ('MANUFACTURER', item['ProducerName']),
+        ('WARRANTY', item['Warranty']),
+        ('ITEM_TYPE', 'product'),
+        ('UNIT', 'ks'),
+        # optional:
+        # ('CATEGORIES', OrderedDict([('CATEGORY', item['CommodityName'])])),
+        ('IMAGES', OrderedDict([('IMAGE', item['ImageUrl'])])),
+        ('FLAGS', OrderedDict([
+            ('ACTION', boolean_to_integer(item['Status'] == 'Doprodej')),
+            ('NEW', boolean_to_integer(item['Status'] == 'Novinka')),
+            ('TIP', boolean_to_integer(item['Status'] == 'TOP Produkt')),
+        ])),
+        ('CODE', item['Code']),
+        ('PRICE', item['EndUserPrice']),
+        # end user price in case of some discount
+        ('STANDARD_PRICE', str(endUserPriceWithVat)),
+        ('PURCHASE_PRICE', str(purchasePriceWithVat)),
+        ('PRICE_VAT', str(endUserPriceWithVat)),
+        # weight is not given in the input data
+        # ('WEIGHT', '0'), # ?
+        ('VAT', str(vatPercent)),
+        ('EAN', eanCode),
+        ('CURRENCY', 'CZK'), # ?
+        ('STOCK', OrderedDict([
+            ('AMOUNT', stockItemCount),
+            ('MINIMAL_AMOUNT', '0'), # ?
+        ])),
+        # default value is 'Skladem' - to allow filtering items on stock
+        ('AVAILABILITY', '' if item['OnStock'] == 'true' else 'Není skladem')
+    ])
+    return out_item
+    
+def round_price(price):
+    'Round price to 2 decimal places'
+    return price.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
+
+def add_vat(price, vat):
+    return round_price(price * (Decimal('1') + vat))
+
+def boolean_to_integer(value):
+    return '1' if value else '0'
+
+def ean13_checksum(code):
+    '''
+    Adds the check digit to a EAN-12 code missing the check digit.
+    code: 12 digit code without the check digit (string)
+    returns: 13 digit code (string)
+
+    It would work analogically for weights '131313131313'.
+    '''
+    weights = '131313131313'
+    s = sum([int(c) * int(w) for (c, w) in zip(code, weights)])
+    check_digit = ((10 - s) % 10) % 10
+    return code + str(check_digit)
+
+def ean14_to_ean_13(code):
+    return ean13_checksum(code[1:-1])
+
+def download_ed_catalog_to_mongo(mongo_uri, catalog_url, counter):
+    mongo = MongoClient(mongo_uri)
+    db = mongo.s3dt_catalog
+    item_collection = db.items
+    item_collection.create_index('code')
+    
+    catalog_xml = download_ed_catalog(catalog_url)
+    load_catalog_to_mongo(catalog_xml, item_collection, counter)
+
+class Counter(object):
+    def __init__(self, report=None, report_period=1000):
+        self.total = 0
+        self.selected = 0
+        self.report_period = report_period
+        self.report = report
+    
+    def item_visited(self):
+        self.total += 1
+        if self.report and self.total % self.report_period == 0:
+            self.report(self)
+    
+    def item_selected(self):
+        self.selected += 1
+    
+    def finished(self):
+        self.report(self)
+
+if __name__ == '__main__':
+    def ed_catalog_url():
+        # catalog_request_url = 'http://public.ws.cz.elinkx.biz/service.asmx/getProductListDownloadZIP'
+        # catalog_url = get_ed_catalog_url(catalog_request_url)
+        catalog_url = 'http://localhost:5000/static/priceList_1055541_UTF8_63e3bbe9-ab87-49bf-a221-b20590b106de.zip'
+        return catalog_url
+    mongo_uri = os.environ.get('MONGOLAB_URI') # localhost if not defined
+    def counter_report(counter):
+        print('total:', counter.total, ', selected:', counter.selected)
+    counter = Counter(report=counter_report, report_period=1000)
+    download_ed_catalog_to_mongo(mongo_uri, ed_catalog_url(), counter)
